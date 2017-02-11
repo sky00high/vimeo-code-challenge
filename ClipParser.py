f8d98b517f5848f87e1c1fc43626f5077bb582af
sky00high <tianyu0726@gmail.com>
2017-02-10 19:00:48 -0500

    Coding Complete

--- ClipParser.py
+++ ClipParser.py
@@ -1,7 +1,18 @@
 import csv
 
+class ClipParserReadError(Exception):
+
+	pass
+
+class ClipParserWriteError(Exception):
+
+	pass
+
+
+
 class ClipParser:
-	
+	requiredColumns = ['id', 'privacy', 'total_likes', 'total_plays', 'title']
+
 	def __init__(self, inputFilename, validFilename="valid.csv", invalidFilename="invalid.csv" ):
 		self.inputFilename = inputFilename
 		self.validFilename = validFilename
@@ -15,45 +26,63 @@ class ClipParser:
 		for item in row:
 			self.indexDict[item] = row.index(item)
 
+		for column in self.requiredColumns:
+			if column not in self.indexDict:
+				throw(ReadError("Missing required column: " + column))
+
+
 	def ifRowValid(self, row):
-		result = True
-		clipPrivacy = row[self.indexDict['privacy']]
-		if clipPrivacy !=  "anybody":
-			result = False
-		clipLikes = int(row[self.indexDict['total_likes']])
-		if clipLikes < 10:
-			result = False
-		clipPlays = int(row[self.indexDict['total_plays']])
-		if clipPlays < 200:
-			result = False
-		clipTitle = row[self.indexDict['title']]
-		if len(clipTitle) > 30:
-			result = False
-		return result
+		try:
+			result = True
+			clipPrivacy = row[self.indexDict['privacy']]
+			if clipPrivacy !=  "anybody":
+				result = False
+			clipLikes = int(row[self.indexDict['total_likes']])
+			if clipLikes < 10:
+				result = False
+			clipPlays = int(row[self.indexDict['total_plays']])
+			if clipPlays < 200:
+				result = False
+			clipTitle = row[self.indexDict['title']]
+			if len(clipTitle) > 30:
+				result = False
+			return result
+		except ValueError:
+			throw(ClipParserReadError("Can not convert field to integer: " + str(row)))
+		except IndexError:
+			throw(ClipParserReadError("Invalid row: " + str(row)))
+
 
 	def validInputFile(self):
-		with open(self.inputFilename, 'r') as inputFile:
+		try:
+			with open(self.inputFilename, 'r') as inputFile:
+				csvReader = csv.reader(inputFile, delimiter=',')
+				titleRow = next(csvReader)
+				self.parseTitleRow(titleRow)
 
-			csvReader = csv.reader(inputFile, delimiter=',')
-			titleRow = next(csvReader)
-			self.parseTitleRow(titleRow)
+				for row in csvReader:
+					clipid = row[self.indexDict['id']]
+					if self.ifRowValid(row):
+						self.validIDs.append([clipid])
+					else:
+						self.invalidIDs.append([clipid])
+		except OSError:
+			throw(ClipParserReadError("filename " + self.inputFilename + " is invalid"))
+		except IndexError:
+			throw(ClipParserReadError("Invalid row"))
 
-			for row in csvReader:
-				clipid = row[self.indexDict['id']]
-				if self.ifRowValid(row):
-					self.validIDs.append([clipid])
-				else:
-					self.invalidIDs.append([clipid])
 
 	def writeOutputFile(self):
+		try:
+			with open(self.validFilename, 'w', newline='') as validIDFile,\
+					open(self.invalidFilename, 'w', newline='') as invalidIDFile:
+				validFileWriter = csv.writer(validIDFile)
+				validFileWriter.writerows(self.validIDs)
 
-		with open(self.validFilename, 'w', newline='') as validIDFile,\
-				open(self.invalidFilename, 'w', newline='') as invalidIDFile:
-			validFileWriter = csv.writer(validIDFile)
-			validFileWriter.writerows(self.validIDs)
-
-			invalidFileWritter = csv.writer(invalidIDFile)
-			invalidFileWritter.writerows(self.invalidIDs)
+				invalidFileWritter = csv.writer(invalidIDFile)
+				invalidFileWritter.writerows(self.invalidIDs)
+		except OSError:
+			throw(ClipParserWriteError("Cannot write to desinated location"))
 
 	def parse(self):
 		self.validInputFile()
--- ValidClips.py
+++ ValidClips.py
@@ -1,9 +1,25 @@
-from ClipParser import ClipParser
+from ClipParser import ClipParser, ClipParserReadError, ClipParserWriteError
+import argparse
 
 if __name__ == "__main__":
 
+	argparser = argparse.ArgumentParser(description = "Parse clip csv file and validate each entry")
+	argparser.add_argument("inputFilename")
+	argparser.add_argument("--validoutputfilename", default="valid.csv", help="destination for valid ids",
+							dest="validOutputFilename")
+	argparser.add_argument("--invalidoutputfilename", default="invalid.csv", help="destination for invalid ids",
+							dest="invalidOutputFilename")
 
-	inputFilename = "clips.csv"
-	parser = ClipParser(inputFilename = inputFilename)
 
-	parser.parse()
+	args = argparser.parse_args()
+
+	try:
+		parser = ClipParser(inputFilename = args.inputFilename,
+							validFilename= args.validOutputFilename,
+							invalidFilename = args.invalidOutputFilename)
+
+		parser.parse()
+	except ClipParserReadError:
+		print("Read error")
+	except ClipParserWriteError:
+		print("write error")
